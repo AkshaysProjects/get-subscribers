9eb9a331d4c88d38f4999b0b63b1f92b6398e217
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2024-02-09 20:49:21 +0530

    Add swagger documentation

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const subscribersRoute = require("./routes/subscribersRoute");
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
 
 // Initialize express application
 const app = express();
@@ -11,6 +13,9 @@ app.get("/", (req, res) => {
 
 // Use the subscribers route
 app.use("/subscribers", subscribersRoute);
+
+// Serve the Swagger UI
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Start the server
 const port = process.env.PORT || 3000;
